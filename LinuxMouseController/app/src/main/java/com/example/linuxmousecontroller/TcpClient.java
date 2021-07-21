e1f1270eb41baa500b54af1b13fb3d8f9e3cf525
NoySartori <noysartory@gmail.com>
2021-07-22 00:28:07 +0300

    remove private members, fix comments

--- LinuxMouseController/app/src/main/java/com/example/linuxmousecontroller/TcpClient.java
+++ LinuxMouseController/app/src/main/java/com/example/linuxmousecontroller/TcpClient.java
@@ -14,8 +14,7 @@ public class TcpClient {
     public static final String TAG = TcpClient.class.getSimpleName();
     public static final String SERVER_IP = "192.168.1.240"; //server IP address
     public static final int SERVER_PORT = 3490;
-    // message to send to the server
-    private String mServerMessage;
+
     // sends message received notifications
     private OnMessageReceived mMessageListener = null;
     // while this is true, the server will continue running
@@ -67,7 +66,6 @@ public class TcpClient {
         mMessageListener = null;
         mBufferIn = null;
         mBufferOut = null;
-        mServerMessage = null;
     }
 
     public void run() {
@@ -75,24 +73,19 @@ public class TcpClient {
         mRun = true;
 
         try {
-            //here you must put your computer's IP address.
             InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
 
             Log.d("TCP Client", "C: Connecting...");
 
-            //create a socket to make the connection with the server
             Socket socket = new Socket(serverAddr, SERVER_PORT);
 
             try {
-
-                //sends the message to the server
                 mBufferOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
-                //receives the message which the server sends back
                 mBufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+                String mServerMessage = "";
 
-                //in this while the client listens for the messages sent by the server
                 while (mRun) {
 
                     mServerMessage = mBufferIn.readLine();
@@ -109,8 +102,7 @@ public class TcpClient {
             } catch (Exception e) {
                 Log.e("TCP", "S: Error", e);
             } finally {
-                //the socket must be closed. It is not possible to reconnect to this socket
-                // after it is closed, which means a new socket instance has to be created.
+                // close socket if something unexpected happened
                 socket.close();
             }
 
@@ -120,8 +112,6 @@ public class TcpClient {
 
     }
 
-    //Declare the interface. The method messageReceived(String message) will must be implemented in the Activity
-    //class at on AsyncTask doInBackground
     public interface OnMessageReceived {
         public void messageReceived(String message);
     }
